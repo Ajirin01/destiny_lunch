79b67497f2476669990c047e2e1a2ff6ffb4c050
Ajirin <mubarakolagoke@gmail.com>
2020-09-27 18:57:15 +0100

    latest and random articles varuble in main

--- dvon_files/resources/views/layouts/site/main_layout.blade.php
+++ dvon_files/resources/views/layouts/site/main_layout.blade.php
@@ -1,3 +1,54 @@
+<?php
+    function getArticlesIndex($article_index, $index){
+        $article_at_index = $article_index[$index];
+        $article_title = strtoupper(preg_replace("/-/"," ",$article_at_index));
+        $data = array('type'=>$article_at_index, 'title'=>$article_title);
+        return $data;
+    }
+    $article_index = [
+            'nigerians-at-home-achievers',
+            'nigerians-in-diaspora-achievers',
+            'notable-profiles',
+            'regional-updates',
+            'disapora-updates',
+            'global-updates',
+            'tribes-and-culture',
+            'agriculture',
+            'mineral-resources',
+            'tourism',
+            'technology-tips',
+            'business-supports',
+            'industrial-development',
+            'made-in-nigeria-products',
+            'exclusive-services',
+            'promotions',
+            'invest-in-nigeria',
+            'not-for-profits',
+            'humanitarian',
+            'destiny-nigeria-development-projects-initiatives',
+        ];
+        $Articles = new App\Article();
+        $random_articles = array();
+        $latest_articles = array();
+        for($i = 0; $i< count($article_index); $i++){
+            $article_at_index = getArticlesIndex($article_index,$i);
+            $articles = $Articles::where('article_type', $article_at_index['type'])->inRandomOrder()->first();
+            $articles = json_decode($articles, true);
+            if($articles != null){
+                array_push($random_articles,$articles);
+            }
+            
+        }
+        for($i = 0; $i< count($article_index); $i++){
+            $article_at_index = getArticlesIndex($article_index,$i);
+            $articles = $Articles::where('article_type', $article_at_index['type'])->latest()->first();
+            $articles = json_decode($articles, true);
+            if($articles != null){
+                array_push($latest_articles,$articles);
+            }
+            
+        }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
